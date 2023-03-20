f0022a45ae0e59787cfe6c5112decd62c96e0f74
RadimYassin <radimyassin20@gamil.com>
2023-03-20 12:25:39 +0100

    updating commit

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const http =require('http')
+const port =6000
+const server=http.createServer(function (req,res) {
+    
+})
 
+
+server.listen(port,function (error) {
+     if (error) {
+                console.log("something went wrong ",error);
+     } else {
+        console.log("server is listening on port ",port);
+     }
+})
 
 
 
